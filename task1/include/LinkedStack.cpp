8ede46a456087acca82b4d2f0e79f6b4eb044252
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2019-09-29 11:45:58 +0300

    Add include files, data member and constructor

--- task1/include/LinkedStack.cpp
+++ task1/include/LinkedStack.cpp
@@ -1,3 +1,12 @@
-class LinkedStack {
+#include "Stack.cpp"
+#include "ListNode.cpp"
 
+template<typename T>
+class LinkedStack : public Stack<T> {
+private:
+	ListNode<T>* top;
+public:
+	LinkedStack() {
+		this->top = nullptr;
+	}
 };
