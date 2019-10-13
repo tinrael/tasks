3997045e3089408d3e2af0d9a643170d39e4e0e1
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2019-10-13 19:17:29 +0300

    Add data members and methods

--- task1/include/ArrayQueue.cpp
+++ task1/include/ArrayQueue.cpp
@@ -11,14 +11,25 @@ private:
 	const int INITIAL_CAPACITY = 4;
 
 	void growCapacity() {
-
+		int count = size();
+		int oldCapacity = capacity;
+		capacity *= 2;
+		T* newItems = new T[capacity];
+		for (int i = 0; i < count; i++) {
+			newItems[i] = items[head];
+			head = (head + 1) % oldCapacity;
+		}
+		head = 0;
+		tail = count;
+		delete[] items;
+		items = newItems;		
 	}
 public:
 	ArrayQueue() {
+		head = 0;
+		tail = 0;
 		items = new T[INITIAL_CAPACITY];
-		capacity = INITIAL_CAPACITY;
-		this->head = 0;
-		this->tail = 0;
+		capacity = INITIAL_CAPACITY;	
 	}
 
 	~ArrayQueue() {
@@ -26,14 +37,27 @@ public:
 	}
 
 	bool isEmpty() {
-		
+		return head == tail;
 	}
 
-	void enqueue(T data) {
-		
+	int size() {
+		return (tail + capacity - head) % capacity;
 	}
 
-	T dequeue() {
-		
+	void enqueue(T data) override {
+		if (size() == capacity - 1) {
+			growCapacity();
+		}
+		items[tail] = data;
+		tail = (tail + 1) % capacity;
+	}
+
+	T dequeue() override {
+		if (isEmpty()) {
+			throw std::logic_error("attempt to dequeue from an empty queue");
+		}
+		T result = items[head];
+		head = (head + 1) % capacity;
+		return result;
 	}
 };
