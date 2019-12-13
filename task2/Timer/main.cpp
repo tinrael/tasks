5a5c60f545a82d8a86fb32b8b463bdf22324c1b4
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2019-12-14 00:50:22 +0200

    Deleted unused include

--- task2/Timer/main.cpp
+++ task2/Timer/main.cpp
@@ -1,14 +1,10 @@
 #include "mainwindow.h"
-#include "customtimer.h"
 
 #include <QApplication>
 #include <QStyleFactory>
-#include <QTime>
 
 int main(int argc, char *argv[])
 {
-    CustomTimer timer(QTime(5, 5));
-
     QApplication::setStyle(QStyleFactory::create("Fusion"));
     QApplication a(argc, argv);
     MainWindow w;
